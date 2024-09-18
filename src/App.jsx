6eb16cbf0f934e76bbceb14f38a6efa3f51bc1a3
Izzat <izzatmuz@gmail.com>
2024-09-18 14:47:11 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { Col, Container, Row } from "react-bootstrap";
+import IconButton from "./components/IconButton";
+import ProfileHeader from "./components/ProfileHeader";
+import { createContext } from "react";
+import { PROFILE_DATA } from "./data";
+import ImageGrid from "./components/ImageGrid";
+
+export const ProfileContext = createContext(null)
+
+export default function App() {
+  return (
+    <ProfileContext.Provider value={PROFILE_DATA}>
+      <Row>
+        <Col
+          sm={1}
+          className='d-flex flex-column justify-content-start align-items-center vh-100 bg-light'
+          style={{ position: "sticky", top: 0 }}
+        >
+          <IconButton className={'bi bi-instagram'} isTop></IconButton>
+          <IconButton className={'bi bi-house'}></IconButton>
+          <IconButton className={'bi bi-search'}></IconButton>
+          <IconButton className={'bi bi-compass'}></IconButton>
+          <IconButton className={'bi bi-film'}></IconButton>
+          <IconButton className={'bi bi-chat'}></IconButton>
+          <IconButton className={'bi bi-heart'}></IconButton>
+          <IconButton className={'bi bi-plus-square'}></IconButton>
+          <IconButton className={'bi bi-person-circle'}></IconButton>
+          <IconButton className={'bi bi-list'} isBottom></IconButton>
+        </Col>
+        <Col sm={11}>
+          <Container>
+            <ProfileHeader />
+            <ImageGrid />
+          </Container>
+        </Col>
+      </Row>
+    </ProfileContext.Provider>
+  )
+}
+
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React, { StrictMode } from 'react'
+import ReactDOM, { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
